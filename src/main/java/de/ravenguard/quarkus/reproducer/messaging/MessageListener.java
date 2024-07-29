87fdd58946162c50453c1b62b97c8dac4553ca62
Bernd Schmidt <bernd.schmidt82@gmx.de>
2024-07-29 21:34:13 +0200

    First commit

---
+++ src/main/java/de/ravenguard/quarkus/reproducer/messaging/MessageListener.java
@@ -0,0 +1,58 @@
+package de.ravenguard.quarkus.reproducer.messaging;
+
+import io.smallrye.mutiny.Uni;
+import io.smallrye.reactive.messaging.kafka.transactions.KafkaTransactions;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.reactive.messaging.*;
+
+@ApplicationScoped
+public class MessageListener {
+  @Channel("out")
+  @OnOverflow(value = OnOverflow.Strategy.BUFFER, bufferSize = 500)
+  KafkaTransactions<Integer> txProducer;
+
+  @Channel("out2")
+  @OnOverflow(value = OnOverflow.Strategy.BUFFER, bufferSize = 500)
+  KafkaTransactions<Integer> txProducer2;
+
+  @Channel("out3")
+  Emitter<Integer> integerEmitter;
+
+  @Incoming("in")
+  @Retry(delay = 1000)
+  public Uni<Void> emitInTransaction(Message<Integer> message) {
+    return txProducer.withTransactionAndAck(message, emitter ->
+        Uni.createFrom()
+            .item(message)
+            .map(Message::getPayload)
+            .chain(payload -> {
+              emitter.send(payload + 1);
+              return Uni.createFrom().voidItem();
+            })
+    );
+  }
+
+  @Incoming("in2")
+  @Retry(delay = 1000)
+  public Uni<Void> emitInTransaction2(Message<Integer> message) {
+    return txProducer2.withTransactionAndAck(message, emitter -> {
+          emitter.send(message.getPayload() + 1);
+          return Uni.createFrom().voidItem();
+        }
+    );
+  }
+
+  @Incoming("in3")
+  @Retry(delay = 1000)
+  public Uni<Void> emitInTransaction3(Message<Integer> message) {
+    return Uni.createFrom()
+        .item(message)
+        .map(Message::getPayload)
+        .flatMap(i -> Uni.createFrom().item(i + 1))
+        .chain(payload -> {
+          integerEmitter.send(payload);
+          return Uni.createFrom().voidItem();
+        });
+  }
+}
---
+++ src/test/java/de/ravenguard/quarkus/reproducer/messaging/MessageListenerTest.java
@@ -0,0 +1,47 @@
+package de.ravenguard.quarkus.reproducer.messaging;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.kafka.InjectKafkaCompanion;
+import io.quarkus.test.kafka.KafkaCompanionResource;
+import io.smallrye.reactive.messaging.kafka.companion.ConsumerTask;
+import io.smallrye.reactive.messaging.kafka.companion.KafkaCompanion;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@QuarkusTest
+@QuarkusTestResource(KafkaCompanionResource.class)
+public class MessageListenerTest {
+
+  @InjectKafkaCompanion
+  KafkaCompanion companion;
+
+  @Test
+  void testProcessor() {
+    companion.produceIntegers().usingGenerator(i -> new ProducerRecord<>("in", i));
+
+    ConsumerTask<String, Integer> orders = companion.consumeIntegers().fromTopics("out", 10);
+    orders.awaitCompletion();
+    assertEquals(10, orders.count());
+  }
+
+  @Test
+  void testProcessor2() {
+    companion.produceIntegers().usingGenerator(i -> new ProducerRecord<>("in2", i));
+
+    ConsumerTask<String, Integer> orders = companion.consumeIntegers().fromTopics("out2", 10);
+    orders.awaitCompletion();
+    assertEquals(10, orders.count());
+  }
+
+  @Test
+  void testProcessor3() {
+    companion.produceIntegers().usingGenerator(i -> new ProducerRecord<>("in3", i));
+
+    ConsumerTask<String, Integer> orders = companion.consumeIntegers().fromTopics("out3", 10);
+    orders.awaitCompletion();
+    assertEquals(10, orders.count());
+  }
+}
